45c11f2539a0cd24265f55c3944d4dcacb65a645
mccullen <mccullen_j@lynchburg.edu>
2019-11-10 13:36:37 -0500

    Updates

--- code/sav.py
+++ code/sav.py
@@ -10,63 +10,55 @@ class SAV():
         with open('20180831.1535680800.warts', 'rb') as f:
             record = warts.parse_record(f)
             i_traceroute_records = 0
-            n_vulnerable = 0
-            vulnerable_as = set()
-            vulnerable_ip = set()
-            all_ip = set()
+            blocked = set()
+            not_blocked = set()
+            customer_as_ids = set()
+            privider_routers = set()
+            vulnerable_routers = set()
+            not_vulnerable_routers = set()
+            n_cases = 0
+
+            total = 0
             while record is not None and i_traceroute_records < 1000:
                 while not isinstance(record, Traceroute):
                     record = warts.parse_record(f)
                 i_traceroute_records += 1
                 print("I: ", i_traceroute_records)
-                #print_addresses(record)
-                #print("Number of hops:", len(record.hops))
-                #print(record.hops)
                 addresses = [record.src_address] + list(map(lambda a: a.address, record.hops))
                 for index, address in enumerate(addresses):
-                    #print("address: ", address)
                     autonomous_system = self.get_as(address)
                     asn = autonomous_system['asn']
-                    #print(autonomous_system)
                     if asn == "NA":
-                        all_ip.add(address)
-                        #if index > 0 and index < len(addresses)-1:
-                        #    # Stub is not first or last address
-                        #    provider_address = addresses[index-1]
-                        #    next_address = addresses[index+1]
-                        #    if provider_address == next_address:
-                        #        print("Found loop!")
-                        #print(f"{index} of {len(addresses) -1}")
-
-                        if index <= len(addresses)-1:
-                            if index == len(addresses)-1:
-                                print("Good")
+                        as_id = self.get_as_id(autonomous_system)
+                        customer_as_ids.add(as_id)
+                        if index > 0 and index < len(addresses)-1:
+                            # Stub is not first or last address
+                            next_address = addresses[index+1]
+                            prev_address = addresses[index-1]
+                            privider_routers.add(next_address)
+                            privider_routers.add(prev_address)
+                            next_as = self.get_as(next_address)
+                            total += 1
+                            if index == len(addresses)-2:
+                                #blocked.add(next_as['asn'])
+                                blocked.add(self.get_as_id(autonomous_system))
+                                not_vulnerable_routers.add(next_address)
+                                print("blocked")
                             else:
-                                remainder = addresses[index+1:]
-                                found = False
-                                i_r = 0
-                                while not found and i_r < len(remainder):
-                                    as2 = self.get_as(remainder[i_r])
-                                    if as2['asn'] != "NA":
-                                        found = True
-                                        found = False
-                                    i_r += 1
-                                if found == True:
-                                    print("Found vulnerability")
-                                    vulnerable_ip.add(address)
-                                else:
-                                    print("Good")
+                                #not_blocked.add(next_as['asn'])
+                                not_blocked.add(self.get_as_id(autonomous_system))
+                                vulnerable_routers.add(next_address)
+                                print("not blocked")
+                                n_cases += 1
+                            print("n vulnerable: ", len(vulnerable_routers))
+                            print("n not vulnerable: ", len(not_vulnerable_routers))
+                            print("n providers: ", len(privider_routers))
+                            print("n cases: ", n_cases)
+                            print("n customers: ", len(customer_as_ids))
 
-
-                        #if index == len(addresses)-1:
-                        #    print("Good")
-                        #else:
-                        #    print("Found vulnerability")
-
+                        print(f"{index} of {len(addresses) -1}")
 
                 record = warts.parse_record(f)
-            print(f"all ip: {len(all_ip)}")
-            print(f"vul ip: {len(vulnerable_ip)}")
 
     def print_addresses(self, record):
         if record.src_address:
@@ -78,5 +70,9 @@ class SAV():
         net = Net(ip_address, allow_permutations=True)
         obj = IPASN(net)
         results = obj.lookup(inc_raw=True)
+        #results['added'] = ASNOrigin(net).lookup(asn=results['asn'])
+
         #pprint(results)
         return results
+    def get_as_id(self, record):
+        return record["asn"] + record["asn_country_code"] + record["asn_date"]
